03b07d6394888d3fdb908af049807f58815b050a
Pavel Koryagin <pavel@koryagin.com>
2017-02-12 03:02:55 +0700

    Fix \extpoint\yii2\base\Controller::checkUrlEndedOnSlash

--- base/Controller.php
+++ base/Controller.php
@@ -2,6 +2,7 @@
 namespace extpoint\yii2\base;
 
 use yii\web\Controller as BaseController;
+use yii\web\ErrorAction;
 use yii\web\HttpException;
 
 class Controller extends BaseController {
@@ -15,15 +16,18 @@ class Controller extends BaseController {
     }
 
     protected function checkUrlEndedOnSlash() {
-        $request = \Yii::$app->request;
-        $url = str_replace('?' . $request->queryString, '', $request->url);
 
-        if (!preg_match('/\/$/', $url)) {
-            if (YII_DEBUG) {
-                throw new HttpException(400, 'This url must be ended on slash `/`, please fix url in link.');
-            } else {
-                $this->redirect($url . '/' . ($request->queryString ? '?' . $request->queryString : ''));
-                return false;
+        if (!($this->action instanceof ErrorAction)) {
+            $request = \Yii::$app->request;
+            $url = str_replace('?' . $request->queryString, '', $request->url);
+
+            if (!preg_match('/\/$/', $url)) {
+                if (YII_DEBUG) {
+                    throw new HttpException(400, 'This url must be ended on slash `/`, please fix url in link.');
+                } else {
+                    $this->redirect($url . '/' . ($request->queryString ? '?' . $request->queryString : ''));
+                    return false;
+                }
             }
         }
 
