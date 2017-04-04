ec3f928df718a9d4bd1cb634cfcfe2b913ea2424
Kozhin Vladimir <affka@affka.ru>
2017-04-04 12:53:17 +0700

    hotfix loader

--- components/ModuleLoader.php
+++ components/ModuleLoader.php
@@ -4,7 +4,8 @@ namespace extpoint\yii2\components;
 
 require_once __DIR__ . '/../base/Module.php';
 
-class ModuleLoader {
+class ModuleLoader
+{
 
     public static $skipFolders = [
         'config',
@@ -12,7 +13,8 @@ class ModuleLoader {
 
     private static $classes;
 
-    public static function getBootstrap($appDir = null) {
+    public static function getBootstrap($appDir = null)
+    {
         $names = [];
         foreach (self::getClasses($appDir) as $name => $moduleClass) {
             if (is_subclass_of($moduleClass, '\yii\base\BootstrapInterface')) {
@@ -22,7 +24,8 @@ class ModuleLoader {
         return $names;
     }
 
-    public static function getConfig($appDir = null) {
+    public static function getConfig($appDir = null)
+    {
         $config = [];
         foreach (self::getClasses($appDir) as $name => $moduleClass) {
             $config[$name] = [
@@ -32,11 +35,18 @@ class ModuleLoader {
         return $config;
     }
 
-    protected static function getClasses($appDir = null, $throw = true) {
+    protected static function getClasses($appDir = null)
+    {
         $appDir = $appDir ?: dirname(dirname(__DIR__)) . '/app';
 
         // Require AppModule class from core
         $path = $appDir . '/core/base/AppModule.php';
+        if (file_exists($path)) {
+            require_once $path;
+        }
+
+        // Require AppAdminModule class from core
+        $path = $appDir . '/core/admin/base/AppAdminModule.php';
         if (file_exists($path)) {
             require_once $path;
         }
@@ -50,30 +60,42 @@ class ModuleLoader {
                 }
 
                 $classPath = $appDir . '/' . $dirName . '/' . ucfirst($dirName) . 'Module.php';
-                if (!file_exists($classPath)) {
-                    if ($throw) {
-                        throw new \Exception('Not found module class file: ' . $classPath);
-                    } else {
-                        continue;
-                    }
-                }
-                require_once $classPath;
+                $className = '\\app\\' . $dirName . '\\' . ucfirst($dirName) . 'Module';
 
-                $className = '\app\\' . $dirName . '\\' . ucfirst($dirName) . 'Module';
-                if (!class_exists($className)) {
-                    throw new \Exception('Not found module class: ' . $className);
-                }
-                if (!is_subclass_of($className, '\extpoint\yii2\base\Module')) {
-                    throw new \Exception('Module class `' . $className . '` is not extends from `\extpoint\yii2\base\Module`');
-                }
-
+                self::loadClass($classPath, $className);
                 self::$classes[$dirName] = $className;
 
                 // Scan submodules
-                static::getClasses($appDir . '/' . $dirName);
+                if (is_dir($appDir . '/' . $dirName)) {
+                    foreach (scandir($appDir . '/' . $dirName) as $subDirName) {
+                        $subClassPath = $appDir . '/' . $dirName . '/' . $subDirName . '/' . ucfirst($dirName) . ucfirst($subDirName) . 'Module.php';
+                        $subClassName = '\\app\\' . $dirName . '\\' . $subDirName . '\\' . ucfirst($dirName) . ucfirst($subDirName) . 'Module';
+
+                        if (file_exists($subClassPath)) {
+                            self::loadClass($subClassPath, $subClassName);
+                            self::$classes[$dirName . '.' . $subDirName] = $className;
+                        }
+                    }
+                }
             }
         }
         return self::$classes;
+    }
+
+    protected static function loadClass($path, $name)
+    {
+        if (!file_exists($path)) {
+            throw new \Exception('Not found module class file: ' . $path);
+        }
+        require_once $path;
+
+        if (!class_exists($name)) {
+            throw new \Exception('Not found module class: ' . $name);
+        }
+        if (!is_subclass_of($name, '\extpoint\yii2\base\Module')) {
+            throw new \Exception('Module class `' . $name . '` is not extends from `\extpoint\yii2\base\Module`');
+        }
+
     }
 
 }
