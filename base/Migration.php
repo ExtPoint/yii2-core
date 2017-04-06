a7f223a0308ffcc3b7b8d2fe47bbfd5aa8a59c47
Kozhin Vladimir <affka@affka.ru>
2017-04-06 10:58:00 +0700

    add foreign key create/delete helpers in Migration class

--- base/Migration.php
+++ base/Migration.php
@@ -30,4 +30,44 @@ class Migration extends \yii\db\Migration
         $this->addPrimaryKey(sprintf('%s_pk', $tableName), $tableName, [$from, $to]);
     }
 
+    /**
+     * @param string $table
+     * @param string|string[] $columns
+     * @param string $refTable
+     * @param string|string[] $refColumns
+     * @param string $delete
+     * @param string $update
+     */
+    public function createForeignKey($table, $columns, $refTable, $refColumns, $delete = 'RESTRICT', $update = 'RESTRICT')
+    {
+        $name = $this->getForeignKeyName($table, $columns, $refTable, $refColumns);
+        parent::addForeignKey($name, $table, $columns, $refTable, $refColumns, $delete, $update);
+    }
+
+    /**
+     * @param string $table
+     * @param string|string[] $columns
+     * @param string $refTable
+     * @param string|string[] $refColumns
+     */
+    public function deleteForeignKey($table, $columns, $refTable, $refColumns)
+    {
+        $name = $this->getForeignKeyName($table, $columns, $refTable, $refColumns);
+        parent::dropForeignKey($name, $table);
+    }
+
+    /**
+     * @param string $table
+     * @param string|string[] $columns
+     * @param string $refTable
+     * @param string|string[] $refColumns
+     * @return string
+     */
+    public static function getForeignKeyName($table, $columns, $refTable, $refColumns)
+    {
+        $columns = implode(',', (array) $columns);
+        $refColumns = implode(',', (array) $refColumns);
+        return sprintf('%s:%s-%s:%s', $table, $columns, $refTable, $refColumns);
+    }
+
 }
