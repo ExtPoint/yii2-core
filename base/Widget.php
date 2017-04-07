1d1ec85314f5f0cdedd160a450cdac86d3a5b280
Kozhin Vladimir <affka@affka.ru>
2017-04-07 12:54:09 +0700

    update bundle name for widgets

--- base/Widget.php
+++ base/Widget.php
@@ -11,15 +11,18 @@ class Widget extends BaseWidget
 {
     public function renderReact($props)
     {
+        // Generate bundle name alias extpoint/yii2-frontend npm package
+        $bundleName = implode('-', array_filter(array_slice(preg_split('/\\/', __CLASS__), -1), function($name) {
+            return preg_match('/[a-z0-9_-]+/', $name) && !in_array($name, ['app', 'widgets']);
+        }));
+
         $jsArgs = [
             Json::encode($this->id),
             Json::encode(get_class($this)),
             !empty($props) ? Json::encode($props) : '{}',
         ];
         $this->view->registerJs('__appWidget.render(' . implode(', ', $jsArgs) . ')', View::POS_END, $this->id);
-
-        $widgetName = substr(strrchr(__CLASS__, "\\"), 1);
-        $this->view->registerJsFile("@static/assets/bundle-$widgetName.js", ['position' => View::POS_END]);
+        $this->view->registerJsFile("@static/assets/bundle-$bundleName.js", ['position' => View::POS_END]);
 
         return Html::tag('span', '', ['id' => $this->id]);
     }
