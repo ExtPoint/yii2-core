72622190eb737dd6ca0d2c744be164c201e22628
Kozhin Vladimir <affka@affka.ru>
2017-12-28 15:32:32 +0700

    hotfix revert

--- middleware/AjaxResponseMiddleware.php
+++ middleware/AjaxResponseMiddleware.php
@@ -1,14 +1,16 @@
 <?php
 
-namespace extpoint\megamenu\middleware;
+namespace extpoint\yii2\middleware;
 
 use extpoint\yii2\base\Controller;
+use extpoint\yii2\traits\ISearchModelTrait;
 use yii\base\ActionEvent;
 use yii\base\Object;
 use yii\web\Application;
-use yii\web\NotFoundHttpException;
+use yii\web\ForbiddenHttpException;
+use yii\web\Response;
 
-class AccessMiddleware extends Object
+class AjaxResponseMiddleware extends Object
 {
     /**
      * @param Application $app
@@ -16,37 +18,55 @@ class AccessMiddleware extends Object
     public static function register($app)
     {
         if ($app instanceof Application) {
-            $app->on(Controller::EVENT_BEFORE_ACTION, [static::className(), 'checkAccess']);
+            $app->on(Controller::EVENT_AFTER_ACTION, [static::className(), 'checkAjaxResponse']);
         }
     }
 
     /**
      * @param ActionEvent $event
-     * @throws NotFoundHttpException
+     * @throws ForbiddenHttpException
      */
-    public static function checkAccess($event)
+    public static function checkAjaxResponse($event)
     {
-        // Skip debug module
-        if ($event->action->controller->module->id === 'debug') {
-            return;
+        $request = \Yii::$app->request;
+        $response = \Yii::$app->response;
+
+        $rawContentType = $request->contentType;
+        if (($pos = strpos($rawContentType, ';')) !== false) {
+            // e.g. application/json; charset=UTF-8
+            $contentType = substr($rawContentType, 0, $pos);
+        } else {
+            $contentType = $rawContentType;
         }
 
-        // Skip error action
-        if ($event->action->uniqueId === \Yii::$app->errorHandler->errorAction) {
-            return;
-        }
+        if ($contentType === 'application/json' && isset($request->parsers[$contentType])) {
 
-        $item = \Yii::$app->megaMenu->getActiveItem();
-        if (!$item) {
-            throw new NotFoundHttpException();
-        }
-        if (!$item->checkVisible($item->normalizedUrl)) {
-            if (\Yii::$app->user->isGuest) {
-                \Yii::$app->user->loginRequired();
+            // Detect data provider
+            if ($event->result instanceof ISearchModelTrait) {
+                $data = $event->result->toFrontend();
+            } else {
+                $data = is_array($event->result) ? $event->result : [];
             }
-            // TODO Show 403?
-            //\Yii::$app->response->redirect(\Yii::$app->homeUrl);
-            $event->isValid = false;
+
+            // Ajax redirect
+            $location = $response->headers->get('Location')
+                ?: $response->headers->get('X-Pjax-Url')
+                    ?: $response->headers->get('X-Redirect');
+            if ($location) {
+                $data['redirectUrl'] = $location;
+                $response->headers->remove('Location');
+                $response->statusCode = 200;
+            } else {
+                // Flashes
+                $session = \Yii::$app->session;
+                $flashes = $session->getAllFlashes(true);
+                if (!empty($flashes)) {
+                    $data['flashes'] = $flashes;
+                }
+            }
+
+            $response->format = Response::FORMAT_JSON;
+            $event->result = $data;
         }
     }
 }
